e8486e2c5ebec99074c1d3414d1bba54c839a941
riyadelberkawy <riyad.elberkawy@gmail.com>
2022-02-16 16:13:09 +0200

    demos

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import "./style.css";
-export { FormikStepper } from "./fromikForm";
-export { FormikStep } from "./fromikForm";
-export type { FormikHelpers } from "formik";
-export * from "./fields";
