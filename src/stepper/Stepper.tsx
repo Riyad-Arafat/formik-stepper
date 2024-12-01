ee3283c7ca5d9294a45ebcd858a3f4afd256c02f
Riyad Arafat <riyad.elberkawy@gmail.com>
2024-12-01 13:34:21 +0200

    Refactor Stepper component for improved type safety and readability; define component type and add displayName

--- src/stepper/Stepper.tsx
+++ src/stepper/Stepper.tsx
@@ -3,7 +3,8 @@ import { FormikStepProps } from "../fromikForm/types";
 import Step from "./Step";
 import { StepperProps } from "./types";
 import "./styles.css";
-const Stepper = ({ activeStep: step, steps }: StepperProps) => {
+
+const Stepper: React.FC<StepperProps> = ({ activeStep: step, steps }) => {
   const [activeStep, setActiveStep] = useState(step);
 
   useEffect(() => {
@@ -11,36 +12,35 @@ const Stepper = ({ activeStep: step, steps }: StepperProps) => {
   }, [step]);
 
   return (
-    <>
-      <div className="stepper-horizontal">
-        {React.Children.map(steps, (child, index) => {
-          if (!React.isValidElement(child)) return null;
-          const { icon, label } = child.props as FormikStepProps;
-          return (
-            <Step
-              key={`step-${index}`}
-              label={label}
-              active={activeStep === index}
-              done={activeStep > index}
-              isFirst={index === 0}
-              isLast={index === React.Children.count(steps) - 1}
-              icon={
-                typeof icon === "function"
-                  ? icon({
-                      active: activeStep === index,
-                      done: activeStep > index,
-                    })
-                  : index + 1
-              }
-            />
-          );
-        })}
-      </div>
-    </>
+    <div className="stepper-horizontal">
+      {React.Children.map(steps, (child, index) => {
+        if (!React.isValidElement(child)) return null;
+        const { icon, label } = child.props as FormikStepProps;
+        return (
+          <Step
+            key={`step-${index}`}
+            label={label}
+            active={activeStep === index}
+            done={activeStep > index}
+            isFirst={index === 0}
+            isLast={index === React.Children.count(steps) - 1}
+            icon={
+              typeof icon === "function"
+                ? icon({
+                    active: activeStep === index,
+                    done: activeStep > index,
+                  })
+                : index + 1
+            }
+          />
+        );
+      })}
+    </div>
   );
 };
 
-export default React.memo(
-  Stepper,
-  (props, nextProps) => props.activeStep === nextProps.activeStep
-);
+Stepper.displayName = "Stepper";
+
+export default React.memo(Stepper, (prevProps, nextProps) => {
+  return prevProps.activeStep === nextProps.activeStep;
+});
